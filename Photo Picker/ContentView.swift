f72a96e4a3a2dbc5cbc4eb6c205a3c4a068e385b
smetankaIa <alex.gg3200@gmail.com>
2024-03-22 20:09:06 +0700

    Initial Commit

---
+++ Photo Picker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Photo Picker
+//
+//  Created by Алексей Никулин on 22.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Photo Picker/Photo_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_PickerApp.swift
+//  Photo Picker
+//
+//  Created by Алексей Никулин on 22.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Photo_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

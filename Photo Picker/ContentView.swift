e93d9fe2ba37363cf0bca8267b2b0f8e6561d465
smetankaIa <alex.gg3200@gmail.com>
2024-03-23 08:41:50 +0700

    Add camera

--- Photo Picker/ContentView.swift
+++ Photo Picker/ContentView.swift
@@ -6,15 +6,60 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+    @State private var isShowIcon = true
+    @State private var isShowPhotoLibrary = false
+    @State private var isShowCamera = false
+    @State private var isConfirmDialog = false
+    @State private var image = UIImage()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Image(uiImage: self.image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 100, height: 100)
+                .onTapGesture {
+                    self.isShowPhotoLibrary = true
+                }
+            VStack {
+                RoundedRectangle(cornerRadius: 50)
+                    .stroke(style: StrokeStyle())
+                    .foregroundColor(.gray)
+                    .frame(width: 100, height: 100)
+                   
+                
+                    .overlay {
+                        if isShowIcon == true {
+                            RoundedRectangle(cornerRadius: 50)
+                                .foregroundColor(.gray)
+                                .frame(width: 100, height: 100)
+                                .onTapGesture {
+                                    isConfirmDialog = true
+                                }
+                        } else {
+                           
+                        }
+                    }
+            }
+            
             Text("Hello, world!")
-        }
+        }.sheet(isPresented: $isShowPhotoLibrary, content: {
+            ImagePicker(selectedImage: self.$image, sourceType: .photoLibrary)
+            
+        })
+        .sheet(isPresented: $isShowCamera, content: {
+            ImagePicker(selectedImage: self.$image, sourceType: .camera)
+        })
+        .confirmationDialog("", isPresented: $isConfirmDialog, actions: {
+            Button("Galery") {
+                isShowPhotoLibrary = true
+            }
+            Button("Camera") {
+                isShowCamera = true
+            }
+        })
         .padding()
     }
 }
--- Photo Picker/ImagePicker.swift
+++ Photo Picker/ImagePicker.swift
@@ -6,3 +6,92 @@
 //
 
 import Foundation
+import UIKit
+import SwiftUI
+
+
+
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Binding var selectedImage: UIImage
+    @Environment(\.presentationMode) private var presentationMode
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        imagePicker.allowsEditing = false
+        return imagePicker
+    }
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+        var parent: ImagePicker
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+    
+    }
+    func makeCoordinator() -> Coordinator {
+        Coordinator( self)
+    }
+    
+}
+    
+    
+
+
+//struct ImagePicker: UIViewControllerRepresentable {
+//    
+//    @Binding var selectedImage: UIImage
+//    @Environment(\.presentationMode) private var presentationMode
+//
+//    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+//
+//    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+//        let imagePicker = UIImagePickerController()
+//        imagePicker.allowsEditing = false
+//        imagePicker.sourceType = sourceType
+//        imagePicker.delegate = context.coordinator
+//
+//        return imagePicker
+//    }
+//
+//    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+//
+//    }
+//    
+//   
+//    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+//     
+//        var parent: ImagePicker
+//     
+//        init(_ parent: ImagePicker) {
+//            self.parent = parent
+//        }
+//     
+//        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//     
+//            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+//                parent.selectedImage = image
+//            }
+//     
+//            parent.presentationMode.wrappedValue.dismiss()
+//        }
+//    }
+//    
+//    func makeCoordinator() -> Coordinator {
+//        Coordinator(self)
+//    }
+//    
+//}
